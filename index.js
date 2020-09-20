d7043995441b546b969cf716c8b51efb8743590a
rolara57 <roger@qm46.com>
2020-09-20 13:52:34 -0500

    starting .js files

---
+++ index.js
@@ -0,0 +1,5 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require(util);
+const api = require("./util/api.js");
+const markdown = require("./utils/generatedMarkdown.js");
